8e689adac40284ef564e7ef9d0c682a09a9d81a3
Takuma Furuya <koya.koya.0729@gmail.com>
2025-04-24 03:45:50 +0000

    :+1: Add sample app

---
+++ apps/sample-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <Button children={<>HOGEHOGE</>} appName={""}></Button>
+      <Button children={<>HOGEHOGE</>} appName={""}></Button>
+      <Button children={<>HOGEHOGE</>} appName={""}></Button>
+      <Button children={<>HOGEHOGE</>} appName={""}></Button>
+    </>
+  );
+}
