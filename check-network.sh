92a0a3a1fd3fd17a8193b4d1d190c90c87f92785
Takuma Furuya <koya.koya.0729@gmail.com>
2025-04-26 16:00:52 +0900

    :recycle: initialize

--- check-network.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-if ! docker network inspect turborepo >/dev/null 2>&1; then
-  echo "Network 'turborepo' does not exist. Creating..."
-  docker network create turborepo
-else
-  echo "Network 'turborepo' already exists."
-fi
